0d16be9b998adebcddf82d35bc6d6a3048a5cfe5
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-10-06 21:12:21 +0300

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,27 +6,25 @@
  */
 int main(void)
 {
-int num1, num2;
-for (num1 = 0; num1 <= 99; num1++)
-{
-for (num2 = num1; num2 <= 99; num2++)
-{
-int tens1 = num1 / 10;
-int units1 = num1 % 10;
-int tens2 = num2 / 10;
-int units2 = num2 % 10;
-putchar(tens1 + '0');
-putchar(units1 + '0');
-putchar(' ');
-putchar(tens2 + '0');
-putchar(units2 + '0');
-if (!(num1 == 99 && num2 == 99))
-{
-putchar(',');
-putchar(' ');
+    int i, j;
+
+    for (i = 0; i < 100; i++)
+    {
+        for (j = i + 1; j < 100; j++)
+        {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(' ');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
+            if (i < 98 || j < 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
+    return (0);
 }
-}
-}
-putchar('\n');
-return (0);
-}
+
