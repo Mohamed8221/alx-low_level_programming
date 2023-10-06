dc3818e1e38286b1a4b7fdc9b4d645f83dea7624
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-10-06 18:16:00 +0300

    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,14 +4,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
 int i;
-for (i = 0; i < 10; i++) {
+for (i = 0; i < 10; i++)
 putchar(i + '0');
-}
-for (i = 'a'; i < 'g'; i++) {
+for (i = 'a'; i < 'g'; i++)
 putchar(i);
-}
 putchar('\n');
-return 0;
+return (0);
 }
