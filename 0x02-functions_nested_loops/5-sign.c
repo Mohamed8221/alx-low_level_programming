71d3dce2a9c5efbfee6f7ed5e9f6f2c4b4094645
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-10-11 00:49:40 +0300

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+%d", n);
+_putchar('+');
 return (1);
 }
 else if (n == 0)
 {
-printf("0");
+_putchar('0');
 return (0);
 }
 else
 {
-printf("-%d", -n);
+_putchar('-');
 return (-1);
 }
 }
