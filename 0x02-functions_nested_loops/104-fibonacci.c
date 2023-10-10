196887808da41ef8b4c2fcc523858694f753872f
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-10-11 02:07:13 +0300

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,8 @@ int main(void)
 int i;
 unsigned long int fib1 = 1, fib2 = 2, fib3;
 
-printf("%lu, %lu", fib1, fib2);
+printf("%lu", fib1);
+printf(", %lu", fib2);
 for (i = 3; i <= 98; i++)
 {
 fib3 = fib1 + fib2;
@@ -23,4 +24,3 @@ printf("\n");
 
 return (0);
 }
-
