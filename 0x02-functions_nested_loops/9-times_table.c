c9bef21766447057aa0e4b5ac8ff906b79102d31
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-10-11 01:09:24 +0300

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,22 +5,25 @@
  */
 void times_table(void)
 {
-int i, j;
-for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-if (j != 0)
-{
+int i, j, product;
+for (i = 0; i <= 9; i++) {
+for (j = 0; j <= 9; j++) {
+product = i * j;
+if (j == 0) {
+_putchar('0');
+}
+else if (product < 10) {
 _putchar(',');
 _putchar(' ');
-}
-if (i * j < 10 && j != 0)
-{
 _putchar(' ');
+_putchar(product + '0');
 }
-_putchar((i * j) / 10 + '0');
-_putchar((i * j) % 10 + '0');
+else {
+_putchar(',');
+_putchar(' ');
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
 }
 _putchar('\n');
 }
