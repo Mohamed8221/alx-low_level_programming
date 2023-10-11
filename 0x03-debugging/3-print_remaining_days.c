fe9b078e00bfd43e52322d75496dd12aec8adafd
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-10-11 23:33:58 +0300

    update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,27 +9,29 @@
 * @year: year
 * Return: void
 */
+
 void print_remaining_days(int month, int day, int year)
 {
-int dayOfYear = convert_day(month, day);
-
-if (is_leap_year(year) && month > 2)
+if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 {
-dayOfYear++;
+if (month > 2 && day >= 60)
+{
+day++;
 }
 
-if (is_leap_year(year) && month == 2 && day == 29)
-{
-printf("Day of the year: %d\n", dayOfYear);
-printf("Remaining days: %d\n", 366 - dayOfYear);
-}
-else if (is_valid_date(month, day, year))
-{
-printf("Day of the year: %d\n", dayOfYear);
-printf("Remaining days: %d\n", 365 - dayOfYear);
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+if (month == 2 && day == 60)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+}
+else
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
+}
 }
 }
