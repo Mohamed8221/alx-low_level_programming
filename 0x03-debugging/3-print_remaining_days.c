453f8d236adc01547226a24edc64a6ae8ade682f
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-10-11 23:01:01 +0300

    update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,30 +9,29 @@
 * @year: year
 * Return: void
 */
+
 void print_remaining_days(int month, int day, int year)
 {
-if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 {
-if (month > 2 || (month == 2 && day == 29))
+if (month > 2 && day >= 60)
 {
-printf("Day of the year: %d\n", 31 + 29 + (month - 3) * 31 - ((4 * month + 23) / 10) + day);
-printf("Remaining days: %d\n", 366 - (31 + 29 + (month - 3) * 31 - ((4 * month + 23) / 10) + day));
+day++;
+}
+
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-}
-}
-else
+if (month == 2 && day == 60)
 {
-if (month == 2 && day == 29)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
-printf("Day of the year: %d\n", (month - 1) * 31 - ((month * 5 - 2) / 10) + day);
-printf("Remaining days: %d\n", 365 - ((month - 1) * 31 - ((month * 5 - 2) / 10) + day));
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
