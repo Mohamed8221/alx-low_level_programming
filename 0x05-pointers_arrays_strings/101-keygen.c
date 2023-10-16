1ee76ccbba2d0d8e13bda8bb609a5b5352ca98cf
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-10-16 23:33:18 +0300

    update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,12 +6,13 @@ int main(void)
 {
 
 srand(time(NULL));
-char password[8];
+char password[9];
 
 for (int i = 0; i < 8; i++)
 {
 password[i] = (rand() % 95) + 32;
 }
+password[8] = '\0';
 printf("%s\n", password);
 return (0);
 
