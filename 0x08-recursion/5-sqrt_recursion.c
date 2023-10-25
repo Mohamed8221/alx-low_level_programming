d546ed9a98000b2c9451a2b4c63f85407d540b32
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-10-25 13:21:07 +0300

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,7 +6,7 @@
 *
 * Return: -1 or sqrt of c
 */
-int sqrt_check(int i, int n)
+int _sqrt_check(int i, int n)
 {
 if (i * i == n)
 {
@@ -16,5 +16,5 @@ if (i * i > n)
 {
 return (-1);
 }
-return (sqrt_check(i + 1, n));
+return (_sqrt_check(i + 1, n));
 }
