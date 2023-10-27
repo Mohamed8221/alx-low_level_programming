f55c85ddc9646a781232c68e89a3f1746fc8fb04
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-10-27 22:26:27 +0200

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Prints the program name
+* @argc: The number of command line arguments
+* @argv: The command line arguments
+*
+* Return: 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+_putchar(argv[0]);
+}
+_putchar('\n')
+return (0);
+}
