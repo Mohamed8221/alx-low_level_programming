97b25127f4e88e251feb1e44865a5056024341d3
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-10-30 17:24:11 +0200

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,47 +1,34 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 #include <string.h>
-#include "main.h"
+
 /**
 * strtow - splits a string into words
-* @str: string to split
+* @str: the string to split
 *
-* Return: pointer to an array of strings (words)
+* Return: a pointer to an array of strings (words)
 */
 char **strtow(char *str)
 {
-char **words;
-int i, j, k, l, m, word_count = 0;
-
-if (str == NULL || str[0] == '\0')
+if (str == NULL || *str == '\0')
 return (NULL);
 
-for (i = 0; str[i]; i++)
-if ((str[i] == ' ' || str[i + 1] == '\0') && str[i + 1] != ' ')
-word_count++;
-
-words = malloc(sizeof(char *) * (word_count + 1));
+char **words = malloc(sizeof(char *));
 if (words == NULL)
 return (NULL);
 
-for (i = 0, l = 0; i < word_count; i++, l++)
+char *token = strtok(str, " ");
+int i = 0;
+while (token != NULL)
 {
-while (str[l] == ' ')
-l++;
-for (j = l; str[j] != ' ' && str[j]; j++)
-;
-words[i] = malloc(sizeof(char) * (j - l + 1));
-if (words[i] == NULL)
-{
-for (; i >= 0; i--)
-free(words[i]);
-free(words);
+words[i] = token;
+i++;
+words = realloc(words, sizeof(char *) * (i + 1));
+if (words == NULL)
 return (NULL);
+token = strtok(NULL, " ");
 }
-for (k = 0, m = l; m < j; k++, m++)
-words[i][k] = str[m];
-words[i][k] = '\0';
-}
-words[word_count] = NULL;
+words[i] = NULL;
+
 return (words);
 }
