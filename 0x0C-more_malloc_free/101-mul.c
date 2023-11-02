910c56c798d43384c07e6febb045f17ff33fa0f0
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-11-03 00:02:26 +0200

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,47 +4,65 @@
 #include <ctype.h>
 
 /**
-* is_number - checks if a string is a number
-* @s: string to check
+* multiply - Multiplies two numbers
+* @num1: The first number
+* @num2: The second number
 *
-* Return: 0 if s is a number, else 1
+* Return: The product of the two numbers
 */
-int is_number(char *s)
+int multiply(int num1, int num2)
 {
-int i = 0;
-
-while (s[i])
-{
-if (!isdigit(s[i]))
-return (1);
-i++;
-}
-
-return (0);
+return (num1 * num2);
 }
 
 /**
-* main - multiplies two positive numbers
-* @argc: argument count
-* @argv: argument vector
+* isPositiveNumber - Checks if a string consists of only digits
+* @str: The string to check
+*
+* Return: 1 if the string is a positive number, 0 otherwise
+*/
+int isPositiveNumber(char *str)
+{
+while (*str)
+{
+if (!isdigit(*str))
+{
+return (0);
+}
+str++;
+}
+return (1);
+}
+
+/**
+* main - Entry point of the program
+* @argc: The number of command line arguments
+* @argv: An array of command line argument strings
 *
 * Return: 0 on success, 98 on error
 */
 int main(int argc, char *argv[])
 {
-long num1, num2, product;
-
-if (argc != 3 || is_number(argv[1]) || is_number(argv[2]))
+if (argc != 3)
 {
 printf("Error\n");
 return (98);
 }
 
-num1 = atol(argv[1]);
-num2 = atol(argv[2]);
-product = num1 *num2;
+char *num1Str = argv[1];
+char *num2Str = argv[2];
 
-printf("%ld\n", product);
+if (!isPositiveNumber(num1Str) || !isPositiveNumber(num2Str))
+{
+printf("Error\n");
+return (98);
+}
+
+int num1 = atoi(num1Str);
+int num2 = atoi(num2Str);
+
+int result = multiply(num1, num2);
+printf("%d\n", result);
 
 return (0);
 }
