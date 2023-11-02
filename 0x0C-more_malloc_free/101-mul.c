a61e1c7a0ba3fd769f3c6104094c70a85c00ecc1
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-11-03 00:21:33 +0200

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,64 +4,29 @@
 #include <ctype.h>
 
 /**
-* main - multiplies two numbers
-* @argc: number of arguments passed to the program
-* @argv: array of arguments passed to the program
-*
-* Return: 0 if successful, 98 if program fails
+* main - multiplies two positive numbers
+* @argc: n arguments
+* @argv: args
+* Return: int
 */
 int main(int argc, char *argv[])
 {
-long int num1, num2;
-long int result;
-
+unsigned long mul;
+int i, j;
 if (argc != 3)
+{ printf("Error\n");
+exit(98); }
+for (i = 1; i < argc; i++)
 {
-errors();
-return (98);
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] > 57 || argv[i][j] < 48)
+{  printf("Error\n");
+exit(98); }
 }
 
-if (!is_digit(argv[1]) || !is_digit(argv[2]))
-{
-errors();
-return (98);
 }
-
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = num1 *num2;
-
-printf("%ld\n", result);
-
+mul = atol(argv[1]) *  atol(argv[2]);
+printf("%lu\n", mul);
 return (0);
-}
-
-/**
-* errors - prints an error message
-*/
-void errors(void)
-{
-printf("Error\n");
-}
-
-/**
-* is_digit - checks if a string only contains digits
-* @s: string to check
-*
-* Return: 1 if s only contains digits, else 0
-*/
-int is_digit(char *s)
-{
-int i = 0;
-
-while (s[i])
-{
-if (!isdigit(s[i]))
-{
-return (0);
-}
-i++;
-}
-
-return (1);
 }
