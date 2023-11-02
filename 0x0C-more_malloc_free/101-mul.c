a4a9e8b689eed455b3ecb2e06b35d9bcee52df42
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-11-03 00:10:49 +0200

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,65 +4,34 @@
 #include <ctype.h>
 
 /**
-* multiply - Multiplies two numbers
-* @num1: The first number
-* @num2: The second number
-*
-* Return: The product of the two numbers
-*/
-int multiply(int num1, int num2)
-{
-return (num1 * num2);
-}
-
-/**
-* isPositiveNumber - Checks if a string consists of only digits
-* @str: The string to check
-*
-* Return: 1 if the string is a positive number, 0 otherwise
-*/
-int isPositiveNumber(char *str)
-{
-while (*str)
-{
-if (!isdigit(*str))
-{
-return (0);
-}
-str++;
-}
-return (1);
-}
-
-/**
-* main - Entry point of the program
-* @argc: The number of command line arguments
-* @argv: An array of command line argument strings
+* main - multiplies two positive numbers
+* @argc: argument count
+* @argv: argument vector
 *
 * Return: 0 on success, 98 on error
 */
 int main(int argc, char *argv[])
 {
+int i, j;
+
 if (argc != 3)
 {
 printf("Error\n");
 return (98);
 }
 
-char *num1Str = argv[1];
-char *num2Str = argv[2];
-
-if (!isPositiveNumber(num1Str) || !isPositiveNumber(num2Str))
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
 return (98);
 }
+}
+}
 
-int num1 = atoi(num1Str);
-int num2 = atoi(num2Str);
-
-int result = multiply(num1, num2);
-printf("%d\n", result);
-
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
