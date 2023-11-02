0bdde658f56c99f78084603d53d6fa6121fd3eea
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-11-03 01:22:22 +0200

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,56 +1,28 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/**
- * multiply - multiplies two positive numbers
- * @num1: first number
- * @num2: second number
- */
-void multiply(char num1[], char num2[])
-{
-	int len1 = strlen(num1);
-	int len2 = strlen(num2);
-	int *result = calloc(len1 + len2, sizeof(int));
-	int i, j;
-	int start;
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			result[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
-			result[i + j] += result[i + j + 1] / 10;
-			result[i + j + 1] %= 10;
-		}
-	}
-
-	/* Print the result */
-	start = result[0] == 0 ? 1 : 0;
-
-	for (i = start; i < len1 + len2; i++)
-		printf("%d", result[i]);
-	printf("\n");
-
-	free(result);
-}
-
 /**
  * main - multiplies two positive numbers
- * @argc: argument count
- * @argv: argument vector
+ * @argc: n arguments
+ * @argv: args
  * Return: int
  */
 int main(int argc, char *argv[])
 {
+unsigned long mul;
+int i, j;
 	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		exit(98);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
+
 	}
-
-	multiply(argv[1], argv[2]);
-
-	return (0);
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
