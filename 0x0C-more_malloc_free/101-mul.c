a42a3d717c83559128da2a1dcea408d1a52e3050
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-11-03 00:13:24 +0200

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,34 +4,60 @@
 #include <ctype.h>
 
 /**
-* main - multiplies two positive numbers
-* @argc: argument count
-* @argv: argument vector
+* _isdigit - checks if a string only contains digits
+* @s: string to check
 *
-* Return: 0 on success, 98 on error
+* Return: 0 if s only contains digits, else 1
+*/
+int _isdigit(char *s)
+{
+int i = 0;
+
+if (s == NULL)
+{
+printf("Error\n");
+exit(98);
+}
+
+while (s[i])
+{
+if (s[i] < '0' || s[i] > '9')
+{
+printf("Error\n");
+exit(98);
+}
+i++;
+}
+
+return (0);
+}
+
+/**
+* main - multiplies two numbers
+* @argc: number of arguments passed to the program
+* @argv: array of arguments passed to the program
+*
+* Return: 0 if successful, 98 if program fails
 */
 int main(int argc, char *argv[])
 {
-int i, j;
+long int num1, num2;
+long int result;
 
 if (argc != 3)
 {
 printf("Error\n");
-return (98);
+exit(98);
 }
 
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (!isdigit(argv[i][j]))
-{
-printf("Error\n");
-return (98);
-}
-}
-}
+_isdigit(argv[1]);
+_isdigit(argv[2]);
 
-printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 *num2;
+
+printf("%ld\n", result);
+
 return (0);
 }
