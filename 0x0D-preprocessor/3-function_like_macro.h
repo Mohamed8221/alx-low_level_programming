da61a7f6f3139e284edc5c0fa6b35a4e211132c4
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-11-06 17:04:22 +0200

    update

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,18 +1,1 @@
-#include <stdio.h>
-
-/**
-* ABS - Computes the absolute value of a number.
-* @x: The number for which to calculate the absolute value.
-*
-* Description: This macro computes the absolute value of a number 'x'.
-*
-* Return: The absolute value of 'x'.
-*/
 #define ABS(x) ((x < 0) ? -(x) : (x))
-
-int main() {
-int num = -5;
-int abs_num = ABS(num);
-printf("The absolute value of %d is %d\n", num, abs_num);
-return (0);
-}
