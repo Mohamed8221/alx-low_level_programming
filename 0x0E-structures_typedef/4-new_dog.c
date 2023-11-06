e2419aa9b23c5030d4827ecd269100a3a0d81f6d
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-11-06 22:05:03 +0200

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,50 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "dog.h"
+
 /**
-* new_dog - Creates a new dog
-* @name: Name of the dog
-* @age: Age of the dog
-* @owner: Owner of the dog
+* new_dog - creates a new dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
 *
-* Return: Pointer to the new dog
+* Return: pointer to the new dog_t
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog;
-char *new_name, *new_owner;
-int name_len = 0, owner_len = 0;
+dog_t *newDog;
+char *cpyName, *cpyOwner;
 
-while (name[name_len])
-name_len++;
-while (owner[owner_len])
-owner_len++;
-
-new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
+newDog = malloc(sizeof(dog_t));
+if (newDog == NULL)
 return (NULL);
 
-new_name = malloc(sizeof(char) * (name_len + 1));
-if (new_name == NULL)
+cpyName = strdup(name);
+if (cpyName == NULL)
 {
-free(new_dog);
+free(newDog);
 return (NULL);
 }
 
-new_owner = malloc(sizeof(char) * (owner_len + 1));
-if (new_owner == NULL)
+cpyOwner = strdup(owner);
+if (cpyOwner == NULL)
 {
-free(new_name);
-free(new_dog);
+free(cpyName);
+free(newDog);
 return (NULL);
 }
 
-for (int i = 0; i <= name_len; i++)
-new_name[i] = name[i];
-for (int i = 0; i <= owner_len; i++)
-new_owner[i] = owner[i];
+newDog->name = cpyName;
+newDog->age = age;
+newDog->owner = cpyOwner;
 
-new_dog->name = new_name;
-new_dog->age = age;
-new_dog->owner = new_owner;
-
-return (new_dog);
+return (newDog);
 }
