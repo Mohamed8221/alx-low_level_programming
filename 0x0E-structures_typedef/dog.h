bbc60821fa837b7ac081b80b609378bb8032bb4e
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-11-06 15:36:28 +0200

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+* struct dog - Defines a dog's attributes
+* @name: The name of the dog.
+* @age: The age of the dog.
+* @owner: The name of the dog's owner.
+*
+* Description: This struct defines the attributes of a dog,
+* including its name, age, and owner's name.
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
