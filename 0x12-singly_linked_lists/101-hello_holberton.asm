bdde6af27abd4465019a0edc1f91f62865771f46
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-11-17 02:17:20 +0200

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,15 @@
 section .data
-    hello db 'Hello, Holberton', 0
+    hello db "Hello, Holberton", 0
 
 section .text
     global main
+
     extern printf
 
 main:
-    ; Prepare the arguments for printf
-    mov rdi, hello     ; The format string
-    xor rax, rax       ; Zero out rax to pass only one argument
-
-    ; Call printf
+    push hello
     call printf
+    add rsp, 8
 
-    ; Return from main
-    mov eax, 0x60      ; The syscall number for sys_exit
-    xor edi, edi       ; Exit code 0
-    syscall            ; Call the kernel
+    mov eax, 0
+    ret
