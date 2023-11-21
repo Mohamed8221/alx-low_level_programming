d2f02a6d58159e0d11a510cd5b33b176443f68da
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-11-21 20:31:01 +0200

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,26 +2,6 @@
 #include <stdio.h>
 
 /**
-* node_is_in_list - checks if a node is in the list
-* @head: pointer to the start of the list
-* @node: node to check
-*
-* Return: 1 if node is in the list, 0 otherwise
-*/
-int node_is_in_list(const listint_t *head, const listint_t *node)
-{
-while (head != NULL)
-{
-if (head == node)
-{
-return (1);
-}
-head = head->next;
-}
-return (0);
-}
-
-/**
 * print_listint_safe - prints a listint_t linked list safely
 * @head: pointer to the start of the list
 *
@@ -30,18 +10,17 @@ return (0);
 size_t print_listint_safe(const listint_t *head)
 {
 size_t len = 0;
-const listint_t *temp = head;
 
-while (temp != NULL)
+while (head)
 {
-printf("[%p] %d\n", (void *)temp, temp->n);
 len++;
-if (node_is_in_list(temp->next, head))
+printf("[%p] %d\n", (void *)head, head->n);
+if ((void *)head < (void *)head->next)
 {
-printf("-> [%p] %d\n", (void *)temp->next, temp->next->n);
+printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 break;
 }
-temp = temp->next;
+head = head->next;
 }
 
 return (len);
