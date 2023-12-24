5d7626c02d8fbdf667ce9bd97dfaa2e5d5df3dc9
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-12-24 18:34:19 +0200

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,63 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <elf.h>
-#include <stdlib.h>
 
 /**
-* main - displays the information contained in the ELF header at the start of an ELF file
+* print_elf_header - prints the ELF header information
+* @header: pointer to the ELF header structure
+*/
+void print_elf_header(Elf32_Ehdr *header)
+{
+int i;
+
+printf("Magic:   ");
+for (i = 0; i < EI_NIDENT; i++)
+printf("%02x ", header->e_ident[i]);
+printf("\n");
+
+
+printf("Version:                           %d (current)\n",
+header->e_ident[EI_VERSION]);
+printf("Entry point address:               0x%x\n", header->e_entry);
+}
+
+/**
+* main - entry point
 * @argc: argument count
 * @argv: argument vector
 *
-* Return: 0 on success, or 98 on error
+* Return: 0 on success, 98 on error
 */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 int fd;
-ssize_t n;
-Elf64_Ehdr ehdr;
+Elf32_Ehdr header;
 
 if (argc != 2)
 {
-dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
-exit(98);
+fprintf(stderr, "Usage: elf_header elf_filename\n");
+return (98);
 }
 
 fd = open(argv[1], O_RDONLY);
 if (fd == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+perror("Error opening file");
+return (98);
 }
 
-n = read(fd, &ehdr, sizeof(ehdr));
-if (n != sizeof(ehdr))
+if (read(fd, &header, sizeof(header)) != sizeof(header))
 {
-dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-close(fd);
-exit(98);
+fprintf(stderr, "Error reading ELF header\n");
+return (98);
 }
 
-if (ehdr.e_ident[EI_MAG0] != ELFMAG0 || ehdr.e_ident[EI_MAG1] != ELFMAG1 ||
-ehdr.e_ident[EI_MAG2] != ELFMAG2 || ehdr.e_ident[EI_MAG3] != ELFMAG3)
+if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 ||
+header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
 {
-dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-close(fd);
-exit(98);
+fprintf(stderr, "Not an ELF file\n");
+return (98);
 }
 
-printf("Magic:   ");
-for (n = 0; n < EI_NIDENT; n++)
-printf("%02x ", ehdr.e_ident[n]);
-printf("\n");
-
-printf("Class:                             %s\n", ehdr.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-printf("Data:                              %s\n", ehdr.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-printf("Version:                           %d (current)\n", ehdr.e_ident[EI_VERSION]);
-printf("OS/ABI:                            UNIX - System V\n");
-printf("ABI Version:                       %d\n", ehdr.e_ident[EI_OSABI]);
-printf("Type:                              %s\n", ehdr.e_type == ET_REL ? "REL (Relocatable file)" : "EXEC (Executable file)");
-printf("Entry point address:               %p\n", (void *)ehdr.e_entry);
+print_elf_header(&header);
 
 close(fd);
 return (0);
