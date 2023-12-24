d2b73cf366ce09fecd82c53922229e9599da8769
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-12-24 16:40:22 +0200

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,9 +18,10 @@ int main(int argc, char *argv[])
 int file_from, file_to, r, w;
 char buffer[BUFFER_SIZE];
 if (argc != 3)
+{
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
-
+}
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
@@ -29,18 +30,21 @@ exit(98);
 }
 file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (file_to == -1)
+{
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 close(file_from);
 exit(99);
+}
 while ((r = read(file_from, buffer, BUFFER_SIZE)) > 0)
 {
 w = write(file_to, buffer, r);
 if (w != r)
+{
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 close(file_from);
 close(file_to);
 exit(99);
-
+}
 }
 if (r == -1)
 {
@@ -55,7 +59,7 @@ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 exit(100);
 }
 if (close(file_to) == -1)
-}
+{
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 exit(100);
 }
