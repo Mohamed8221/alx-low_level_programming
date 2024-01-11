a6f7546852282450890d24ff5ba54a722c821aea
Mohamed8221 <s21511383eg@std.aou.edu.eg>
2024-01-12 01:05:10 +0200

    update

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,51 +3,50 @@
 #include <string.h>
 
 /**
-* main - Generates and prints passwords for a given executable.
+* main - Generates and prints passwords for the crackme5 executable.
 * @argc: The number of arguments supplied to the program.
 * @argv: An array of pointers to the arguments.
 * Return: Always 0.
 */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-char pass[7], *code;
-int length, i, temp;
-if (argc != 2)
+char password[7], *codex;
+int len = strlen(argv[1]), i, tmp;
+
+codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+
+tmp = (len ^ 59) & 63;
+password[0] = codex[tmp];
+
+tmp = 0;
+for (i = 0; i < len; i++)
+tmp += argv[1][i];
+password[1] = codex[(tmp ^ 79) & 63];
+
+tmp = 1;
+for (i = 0; i < len; i++)
+tmp *= argv[1][i];
+password[2] = codex[(tmp ^ 85) & 63];
+
+tmp = 0;
+for (i = 0; i < len; i++)
 {
-fprintf(stderr, "Usage: %s username\n", argv[0]);
-return (1);
+if (argv[1][i] > tmp)
+tmp = argv[1][i];
 }
-code = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-length = strlen(argv[1]);
-temp = (length ^ 59) & 63;
-pass[0] = code[temp];
-temp = 0;
-for (i = 0; i < length; i++)
-temp += argv[1][i];
-pass[1] = code[(temp ^ 79) & 63];
-temp = argv[1][0];
-for (i = 0; i < length; i++)
-temp *= argv[1][i];
-pass[2] = code[(temp ^ 85) & 63];
-temp = argv[1][0];
-for (i = 0; i < length; i++)
-if (argv[1][i] > temp)
-temp = argv[1][i];
-srand(temp ^ 14);
-pass[3] = code[rand() & 63];
+srand(tmp ^ 14);
+password[3] = codex[rand() & 63];
 
-temp = argv[1][0];
-for (i = 0; i < length; i++)
-temp += (argv[1][i] * argv[1][i]);
-pass[4] = code[(temp ^ 239) & 63];
+tmp = 0;
+for (i = 0; i < len; i++)
+tmp += (argv[1][i] * argv[1][i]);
+password[4] = codex[(tmp ^ 239) & 63];
 
-temp = rand();
 for (i = 0; i < argv[1][0]; i++)
-temp = rand();
-pass[5] = code[(temp ^ 229) & 63];
+tmp = rand();
+password[5] = codex[(tmp ^ 229) & 63];
 
-pass[6] = '\0';
-printf("%s", pass);
-
+password[6] = '\0';
+printf("%s", password);
 return (0);
 }
