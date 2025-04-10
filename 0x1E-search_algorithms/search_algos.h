c7ff962d5d33ab462198627ed6f0ee544c4b4dd2
mohamed <mohamed@localhost.localdomain>
2025-04-10 02:00:42 +0200

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,12 +4,24 @@
 #include <stddef.h>
 
 /**
+ * struct listint_s - singly linked list node
+ * @n: Integer stored
+ * @index: Node index in list
+ * @next: Pointer to next node
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/**
  * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
+ * @n: Integer stored
+ * @index: Node index
+ * @next: Pointer to next node
+ * @express: Pointer to next node in express lane
  */
 typedef struct skiplist_s
 {
@@ -19,13 +31,14 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
+/* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* SEARCH_ALGOS_H */
